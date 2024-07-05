24b59b961f93b302e1806eefc3a079399f2d2513
KrishnaG Thakur <kgopa0204@gmail.com>
2024-07-05 14:40:04 +0530

    add database for registrations

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request
+from data import Database
 
 app = Flask(__name__)
-
+db = Database()
 @app.route("/")
 def index():
     return render_template('login.html')
@@ -15,6 +16,12 @@ def perform_registration():
     name = request.form.get('user_name')                  # for recieving the data by post method
     email = request.form.get('user_email')
     password = request.form.get('user_password')
-    return name + " " + email + " " + password
+
+    response = db.insert(name,email,password)
+
+    if response:
+        return "Registration Successful"
+    else:
+        return "Email already exists"
 
 app.run(debug=True)
