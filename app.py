8f8029a7950a1a2db62e925ac296e73f7c5e8481
KrishnaG Thakur <kgopa0204@gmail.com>
2024-07-05 08:58:54 +0530

    add registration page

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 def index():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('registration.html')
+
 app.run(debug=True)
